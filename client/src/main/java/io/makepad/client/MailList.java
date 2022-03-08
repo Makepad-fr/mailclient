7e773de542693e77e8f78d8eb9347f6ecd312175
idilsaglam <idilsaglam1@hotmail.com>
2022-03-08 15:31:06 +0100

    gui added

---
+++ client/src/main/java/io/makepad/client/MailList.java
@@ -0,0 +1,43 @@
+package io.makepad.client;
+
+import com.sun.source.tree.Tree;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
+
+public class MailList extends TreeView<String> {
+    MailList(){
+        TreeItem<String> rootItem = new TreeItem<String>("Mail list");
+
+        TreeItem<String> mail1 = new TreeItem<String>("idilsaglam1@hotmail.com");
+        mail1.getChildren().add(new TreeItem<String>("Inbox"));
+        mail1.getChildren().add(new TreeItem<String>("Drafts"));
+        mail1.getChildren().add(new TreeItem<String>("Sent"));
+        mail1.getChildren().add(new TreeItem<String>("Deleted"));
+        mail1.getChildren().add(new TreeItem<String>("Spam"));
+        mail1.getChildren().add(new TreeItem<String>("Archive"));
+        rootItem.getChildren().add(mail1);
+
+        TreeItem<String> mail2 = new TreeItem<String>("idil.saglam@makepad.fr");
+        mail2.getChildren().add(new TreeItem<String>("Inbox"));
+        mail2.getChildren().add(new TreeItem<String>("Drafts"));
+        mail2.getChildren().add(new TreeItem<String>("Sent"));
+        mail2.getChildren().add(new TreeItem<String>("Deleted"));
+        mail2.getChildren().add(new TreeItem<String>("Spam"));
+        mail2.getChildren().add(new TreeItem<String>("Archive"));
+        rootItem.getChildren().add(mail2);
+
+        TreeItem<String> mail3 = new TreeItem<String>("idil.saglam@etu.u-paris.fr");
+        mail3.getChildren().add(new TreeItem<String>("Inbox"));
+        mail3.getChildren().add(new TreeItem<String>("Drafts"));
+        mail3.getChildren().add(new TreeItem<String>("Sent"));
+        mail3.getChildren().add(new TreeItem<String>("Deleted"));
+        mail3.getChildren().add(new TreeItem<String>("Spam"));
+        mail3.getChildren().add(new TreeItem<String>("Archive"));
+        rootItem.getChildren().add(mail3);
+
+        this.setRoot(rootItem);
+        this.setShowRoot(false);
+    }
+
+}
---
+++ client/src/main/java/io/makepad/client/Main.java
@@ -0,0 +1,28 @@
+package io.makepad.client;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        MailList l = new MailList();
+        Scene scene = new Scene(new VBox(l));
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+}
